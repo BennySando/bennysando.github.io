3444615d988892300e5525f26f29807f9afe3e8e
BennySando <100108031+BennySando@users.noreply.github.com>
2022-12-29 14:56:28 -0600

    test

--- script_test.js
+++ script_test.js
@@ -1,3 +1,4 @@
 function bruhify() {
     document.getElementById("text").innerHTML="bruh";
+    document.getElementByID("testImage").src="https://static.displate.com/280x392/displate/2020-07-08/cd0478f68cdb953e2bbe012c4035c038_e0b3c1e83e0066602233af5fef7df4df.jpg"
 }
