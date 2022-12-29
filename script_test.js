6fd682bd5f4c62a8f5f01d7be105090138f58e5d
BennySando <100108031+BennySando@users.noreply.github.com>
2022-12-29 15:15:59 -0600

    test

--- script_test.js
+++ script_test.js
@@ -1,4 +1,7 @@
 function bruhify() {
+    //How to change text
     document.getElementById("text").innerHTML="bruh";
+
+    //How to change an image
     document.getElementById("testImage").src="https://static.displate.com/280x392/displate/2020-07-08/cd0478f68cdb953e2bbe012c4035c038_e0b3c1e83e0066602233af5fef7df4df.jpg"
 }
