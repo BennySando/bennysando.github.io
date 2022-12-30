b18b81ff93d96ed4459d760f5e889d7ec994bf33
BennySando <100108031+BennySando@users.noreply.github.com>
2022-12-30 17:31:35 -0600

    test

--- script_test.js
+++ script_test.js
@@ -6,7 +6,13 @@ function bruhify() {
     document.getElementById("testImage").src="https://static.displate.com/280x392/displate/2020-07-08/cd0478f68cdb953e2bbe012c4035c038_e0b3c1e83e0066602233af5fef7df4df.jpg"
 
     //How to get values from radio buttons
-    let val = document.getElementsByName("buttons").values()
+    let buttons = document.getElementsByName("buttons")
+    for (var i = 0; i < buttons.length; i++){
+        if (buttons[i].checked){
+            let val = buttons[i];
+        }
+        break;
+    }
     if (val == 0){
         document.getElementById("text").color = red;
     } else if (val == 1){
