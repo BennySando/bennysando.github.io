d07241c4910145d8d52d0b3a2ec9cb936f32cb0a
BennySando <100108031+BennySando@users.noreply.github.com>
2022-12-30 15:55:32 -0600

    Update script_test.js

--- script_test.js
+++ script_test.js
@@ -1,6 +1,6 @@
 function bruhify() {
     //How to change text
-    document.getElementById("text").innerHTML="bruh";
+    document.getElementById("text").innerHTML="bruh" + " bruh but again";
 
     //How to change an image
     document.getElementById("testImage").src="https://static.displate.com/280x392/displate/2020-07-08/cd0478f68cdb953e2bbe012c4035c038_e0b3c1e83e0066602233af5fef7df4df.jpg"
