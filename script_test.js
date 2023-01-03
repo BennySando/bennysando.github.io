504a6f0c4c28196751e020a696df93cfe3174620
BennySando <100108031+BennySando@users.noreply.github.com>
2023-01-03 16:50:19 -0600

    Update script_test.js

--- script_test.js
+++ script_test.js
@@ -1,6 +1,9 @@
 function bruhify() {
     //How to change text
     document.getElementById("text").innerHTML="bruh" + " bruh but again";
+    
+    //Testing whether or not you can add 1 to p elements
+    document.getElementById("number").innerHTML = document.getElementById("number").innerHTML + 1;
 
     //How to change an image
     document.getElementById("testImage").src="https://static.displate.com/280x392/displate/2020-07-08/cd0478f68cdb953e2bbe012c4035c038_e0b3c1e83e0066602233af5fef7df4df.jpg"
